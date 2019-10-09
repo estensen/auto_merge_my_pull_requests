06a2282e9b6b45e6d151fef46135ea16479e9d4a
Håvard Anda Estensen <haavard.ae@gmail.com>
2019-10-09 11:25:20 +0200

    Print event data

--- merge_and_cleanup_branch.py
+++ merge_and_cleanup_branch.py
@@ -48,6 +48,8 @@ if __name__ == '__main__':
 
     github_event_path = os.environ["GITHUB_EVENT_PATH"]
     event_data = json.load(open(github_event_path))
+    
+    print(event_data)
 
     check_run = event_data["check_run"]
     name = check_run["name"]
